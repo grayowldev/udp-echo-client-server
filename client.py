90250f20c30f34a51a46a90c76f78db9414bf0f1
grayowldev <truegrayowl@gmail.com>
2018-10-23 17:01:32 -0400

    client and server files added

---
+++ client.py
@@ -0,0 +1,43 @@
+#Kwasi Nsiah
+#KON2
+
+import sys, time
+from socket import *
+
+# Get the server hostname, port and data length as command line arguments
+argv = sys.argv
+host = argv[1]
+port = argv[2]
+count = argv[3]
+
+# Command line argument is a string, change the port and count into integer
+port = int(port)
+count = int(count)
+data = 'X' * count # Initialize data to be sent
+
+# Create UDP client socket. Note the use of SOCK_DGRAM
+clientsocket = socket(AF_INET, SOCK_DGRAM)
+
+# Set client timeout to a second 
+clientsocket.settimeout(1)
+
+numAttempts = 0
+while numAttempts < 3:
+    try:
+        # Sending data to server
+        print("Sending data to " + host + ", " + str(port) + ": " + data)
+        clientsocket.sendto(data.encode(),(host, port))
+
+        # Receive the server response
+        dataEcho, address = clientsocket.recvfrom(count)
+
+        # Display the server response as an output
+        print("Receive data from " + address[0] + ", " + str(address[1]) + ": " + dataEcho.decode())
+
+        #Close the client socket
+        clientsocket.close()
+
+        numAttempts = 6
+    except timeout:
+        print("Message timed out")
+        numAttempts += 1
